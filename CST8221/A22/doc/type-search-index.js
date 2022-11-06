745f326f98d7cb91052556a8276a23ee86bc2320
Akshit Sabharwal <sabh0009@algonquinlive.com>
2022-11-06 15:43:08 -0500

    Added A21

---
+++ CST8221/A22/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"numPuzzle","l":"GameController"},{"p":"numPuzzle","l":"GameGrid"},{"p":"numPuzzle","l":"GameModel"},{"p":"numPuzzle","l":"GameView"},{"p":"numPuzzle","l":"Main"}];updateSearchResults();
